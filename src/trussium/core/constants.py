0266ca77bc22cfed086fda801c6a19f28611328f
sartim <sarrtim@gmail.com>
2026-07-23 02:04:03 +0300

    Add constants.py

---
+++ src/trussium/core/constants.py
@@ -0,0 +1,12 @@
+"""
+Shared project constants.
+"""
+
+APP_NAME = "Trussium"
+APP_DESCRIPTION = "Cloud-Native AI Runtime Platform"
+APP_VERSION = "0.1.0"
+
+DEFAULT_GRPC_PORT = 50051
+DEFAULT_HTTP_PORT = 8080
+
+DEFAULT_LOG_LEVEL = "INFO"
